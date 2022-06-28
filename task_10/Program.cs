c196b9c703132c2ceeeaa5d13acf0e4f77d7c934
ivan <lopatin.ivan28@icloud.com>
2022-06-28 20:04:19 +0300

    add task 10

---
+++ task_10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, отображающая вторую цифру трёхзначного числа
+
+string secondNumber(int number)
+{
+    if (number >= 100 && number <= 999)
+    {
+        int second = (number%100)/10;
+        return ("Вторая цифра: " + second);
+    }
+    else
+        return "Введённое число не является трехзначным";
+}
+
+Console.WriteLine("Введите трёхзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(secondNumber(num));
