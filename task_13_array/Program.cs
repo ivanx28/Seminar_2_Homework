4356fda227f6a848804350cba16bf632e59e3ace
ivan <ivan@vn.com>
2022-07-01 11:47:32 +0300

    add task 13 by array

---
+++ task_13_array/Program.cs
@@ -0,0 +1,20 @@
+﻿// Отображение третьей цифры любого числа 
+// вариант решения с помощью массива
+
+Console.WriteLine ("Введите число");
+
+string number = Console.ReadLine();
+
+string thirdNumber()
+{
+    if (number.Length < 3)
+    {
+        return "Третья цифра в ведённом числе отсутствует";
+    }
+    else
+    {
+        return "Третья цифра введённого числа " + number[2];
+    }
+}
+
+Console.WriteLine(thirdNumber());
